fa77cd851d8df9d8d51b5addd8e42f8d9b1cc9a7
hpetrovych <gordy.cp96@gmail.com>
2022-03-11 12:08:34 +0200

    cleanup parallel

--- upload-tiles/cleanup.sh
+++ upload-tiles/cleanup.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
-for f in $(find ./tiles-dest -type f); do 
-    if [ ! -z "$(magick identify -regard-warnings $f 2>&1 > /dev/null)" ]; then 
-        size=$(wc -c <"$f")
-        is404=$(grep -c 'Error 404 (Not Found)' $f)
-        if [ $size -eq 0 ]; then
-            echo "$f is empty"
+function scan {
+    echo "scanning $1"
+    for f in $(find $1 -type f); do 
+        if [ ! -z "$(magick identify -regard-warnings $f 2>&1 > /dev/null)" ]; then 
+            size=$(wc -c <"$f")
+            is404=$(grep -c 'Error 404 (Not Found)' $f)
+            if [ $size -eq 0 ]; then
+                echo "$f is empty"
+            fi
+            if [ $is404 -eq 0 ] && [ $size -gt 0 ]; then 
+                rm $f
+                echo "removing $f"; 
+            fi
         fi
-        if [ $is404 -eq 0 ] && [ $size -gt 0 ]; then 
-            rm $f
-            echo "removing $f"; 
-        fi
-    fi
+    done
+}
+trap 'killall' INT
+
+killall() {
+    trap '' INT TERM
+    echo "**** Shutting down... ****"
+    kill -TERM 0
+    wait
+    echo DONE
+}
+for d in $(ls ./tiles-dest); do
+    scan "./tiles-dest/$d" &
 done
+wait
