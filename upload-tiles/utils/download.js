d9f4fe1bb927ff7524660b3d884edd74315f470c
hpetrovych <gordy.cp96@gmail.com>
2022-03-13 09:56:43 +0200

    osm 418 response fixed by adding more hUmAn headers

--- upload-tiles/utils/download.js
+++ upload-tiles/utils/download.js
@@ -47,8 +47,21 @@ const download = (resource, locationParams, cb = () => {}) => {
       {
         timeout: 5000,
         headers: {
+          "accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+          "accept-encoding": "gzip, deflate, br",
+          "accept-language": "uk,en-US;q=0.9,en;q=0.8,ru;q=0.7",
+          "cache-control": "no-cache",
+          "pragma": "no-cache",
+          "sec-ch-ua": `" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"`,
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": `"macOS"`,
+          "sec-fetch-dest": "document",
+          "sec-fetch-mode": "navigate",
+          "sec-fetch-site": "none",
+          "sec-fetch-user": "?1",
+          "upgrade-insecure-requests": 1,
           "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
         },
       },
       function (response) {
